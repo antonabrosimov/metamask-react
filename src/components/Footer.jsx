41b7e7465e29268fcb073b58fe5bf662ef25a2a1
antonabrosimov <antonabrosimov299@gmail.com>
2021-12-13 23:51:33 +0800

    fix bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Tabs = () => (
   <div className="pp-footer">
     <div className="container">
       <div className="d-flex justify-content-between">
-        <div className="text-muted"> Copyright © 2021. All Rights Reserved by MetaVPad</div>
+        <div className="text-muted">
+          Copyright © 2021. All Rights Reserved by MetaVPad
+        </div>
         <div className="text-muted"> v1.2.9</div>
         <div className="text-end pp-footer-link">
-          <a href="https://bscpad.com/privacy-policy" target="_blank" className="back-top">Privacy
-            Policy</a>
-          <a href="https://bscpad.com/terms-of-use" target="_blank" className="back-top">Terms of Use</a>
+          <a
+            href="https://bscpad.com/privacy-policy"
+            target="_blank"
+            className="back-top"
+            rel="noopener noreferrer"
+          >
+            Privacy Policy
+          </a>
+          <a
+            href="https://bscpad.com/terms-of-use"
+            target="_blank"
+            className="back-top"
+            rel="noopener noreferrer"
+          >
+            Terms of Use
+          </a>
         </div>
       </div>
     </div>
