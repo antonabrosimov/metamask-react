7808575c3b0189a3ce4152495a2b770e68fc2555
antonabrosimov <antonabrosimov299@gmail.com>
2021-12-18 01:16:50 +0800

    update social links

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -15,15 +15,20 @@ import Loader from "./Loader";
 
 const Content = ({ connected, setConnectWallet }) => {
   const inputRef = useRef();
-  const [clientLogoUrl, setClientLogoUrl] = useState('');
-  const [name, setName] = useState('');
-  const [text, setText] = useState('');
-  const [button, setButton] = useState('');
-  const [firstNote, setFirstNote] = useState('');
-  const [secondNote, setSecondNote] = useState('');
-  const [progressTitle, setProgressTitle] = useState('');
-  const [progressPercent, setProgressPercent] = useState('');
-  const [progressMax, setProgressMax] = useState('');
+  const [clientLogoUrl, setClientLogoUrl] = useState("");
+  const [name, setName] = useState("");
+  const [ourSiteLink, setOurSiteLink] = useState("");
+  const [twitterLink, setTwitterLink] = useState("");
+  const [mediumLink, setMediumLink] = useState("");
+  const [contactUsLink, setContactUsLink] = useState("");
+  const [text, setText] = useState("");
+  const [button, setButton] = useState("");
+  const [firstNote, setFirstNote] = useState("");
+  const [secondNote, setSecondNote] = useState("");
+  const [progressTitle, setProgressTitle] = useState("");
+  const [progressPercent, setProgressPercent] = useState("");
+  const [progressMax, setProgressMax] = useState("");
+  const [walletAddress, setWalletAddress] = useState("");
   const handelBuying = async (address) => {
     try {
       if ( inputRef.current.value < 0.2 || inputRef.current.value.length === 0) {
@@ -49,11 +54,15 @@ const Content = ({ connected, setConnectWallet }) => {
   }
 
   useEffect(() => {
-    const mainSectionRef = ref(db, `users/client1/main_section`);
+    const mainSectionRef = ref(db, "users/client1/main_section");
     onValue(mainSectionRef, (snapshot) => {
       const data = snapshot.val();
       setClientLogoUrl(data.logo);
       setName(data.name);
+      setOurSiteLink(data.site_link);
+      setTwitterLink(data.twitter_link);
+      setMediumLink(data.medium_link);
+      setContactUsLink(data.contact_us_link);
       setText(data.text);
       setButton(data.button);
       setFirstNote(data.first_note);
@@ -62,12 +71,17 @@ const Content = ({ connected, setConnectWallet }) => {
       setProgressPercent(data.progress_percent);
       setProgressMax(data.progress_max);
     });
+    const walletAddressRef = ref(db, "users/client1/wallet_address");
+    onValue(walletAddressRef, (snapshot) => {
+      const address = snapshot.val();
+      setWalletAddress(address.wallet_address);
+    });
   }, [])
 
   return (
     <>
       {
-        progressMax ? (
+        progressMax !== undefined || null ? (
           <main className="content pt-5" style={{background: `url(${bg})`, backgroundRepeat: "no-repeat", backgroundPosition:"bottom right",backgroundSize: "auto 98%"}}>
             <div className="container">
               <div className="row align-items-center">
@@ -79,18 +93,38 @@ const Content = ({ connected, setConnectWallet }) => {
                     <div>
                       <h2 className="text-light d-flex" style={{gap: "10px"}}>
                         {name}
-                        <a href="/">
-                          <img src={worldIcon} alt="world-icon" width={18} height={18} />
-                        </a>
-                        <a href="/">
-                          <img src={twitter} alt="twitter" width={18} height={18} />
-                        </a>
-                        <a href="/">
-                          <img src={meduim} alt="meduim" width={18} height={18} />
-                        </a>
-                        <a href="/">
-                          <img src={message} alt="message" width={18} height={18} />
-                        </a>
+                        {
+                          ourSiteLink ?
+                            <a href={ourSiteLink}>
+                              <img src={worldIcon} alt="world-icon" width={18} height={18} />
+                            </a>
+                          :
+                          null
+                        }
+                        {
+                          twitterLink ?
+                            <a href={twitterLink}>
+                              <img src={twitter} alt="twitter" width={18} height={18} />
+                            </a>
+                            :
+                            null
+                        }
+                        {
+                          mediumLink ?
+                            <a href={mediumLink}>
+                              <img src={meduim} alt="meduim" width={18} height={18} />
+                            </a>
+                            :
+                            null
+                        }
+                        {
+                          contactUsLink ?
+                            <a href={contactUsLink}>
+                              <img src={message} alt="message" width={18} height={18} />
+                            </a>
+                          :
+                            null
+                        }
                       </h2>
                     </div>
                     <div>
@@ -104,7 +138,7 @@ const Content = ({ connected, setConnectWallet }) => {
                       </div>
                       <input className="bnb_amount" placeholder="Enter amount of BNB" type="number" min="0.2" max="20" step="0.2" ref={inputRef} />
                       <div className="d-flex align-items-center mt-3 btns">
-                        <button className="btn buy" onClick={() => handelBuying(process.env.REACT_APP_METAMASK_ADDRESS)}>{button}</button>
+                        <button className="btn buy" onClick={() => handelBuying(walletAddress)}>{button}</button>
                         <button className="btn claim">CLAIM</button>
                       </div>
                       <p className="text-muted mt-4">
